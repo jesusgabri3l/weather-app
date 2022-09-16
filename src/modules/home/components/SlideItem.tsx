8049803b028b75101010e2593bf82da55b3d03eb
Jesús Gabriel <jesus15202009@gmail.com>
2022-09-16 01:01:02 -0500

    response

--- src/modules/home/components/SlideItem.tsx
+++ src/modules/home/components/SlideItem.tsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 function SlideItem({ location }: any) {
-  console.log(location);
   const [weather, setWeather] = useState<any>(null);
   const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
@@ -22,12 +21,12 @@ function SlideItem({ location }: any) {
     if (temp > 30) return 'text-hot';
   };
   return (
-    <div className="bg-element p-8 rounded-lg card flex">
-      <div className="w-3/5">
-        <h3 className="text-white text-3xl font-bold tracking-wide truncate w-full">
+    <div className="p-6 pb-16 rounded-lg card md:p-8">
+      <div className="w-full md:w-3/5">
+        <h3 className="text-white text-2xl font-bold tracking-wide truncate w-full md:text-3xl">
           {location.basicInfo.address_components[0].long_name}
         </h3>
-        <p className="text-gray font-bold text-xl truncate w-full">
+        <p className="text-gray font-bold text-lg truncate w-full md:text-xl">
           {location.basicInfo.address_components[2].long_name}
         </p>
         {location.basicInfo.address_components.length > 3 && (
@@ -37,46 +36,54 @@ function SlideItem({ location }: any) {
         )}
       </div>
       {weather && (
-        <div className="w-2/5 flex flex-col items-end">
-          <div className="flex items-center">
-            <h3 className="text-white text-2xl font-bold tracking-wide truncate w-4/5 inline-block">
-              {weather.weather[0].main}
-            </h3>
-            <img
-              src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-              alt="illustration"
-              className="inline"
-              width="80"
-              height="80"
-            />
+        <div className="w-full md:w-2/5">
+          <div className="flex items-end md:flex-col">
+            <div className="flex items-center">
+              <h3 className="text-white text-xl font-bold tracking-wide truncate w-4/5 inline-block md:text-2xl">
+                {weather.weather[0].main}
+              </h3>
+              <img
+                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt="illustration"
+                className="inline"
+                width="80"
+                height="80"
+              />
+            </div>
           </div>
-          <div className="flex items-center">
-            <p className="text-gray text-base font-medium">Temperature: </p>
-            <p
-              className={`text-xl ml-2 font-bold ${getColorByTemperature(
-                weather.main.temp,
-              )}`}
-            >
-              {weather.main.temp}° c
-            </p>
-          </div>
-          <div className="flex items-center mt-2">
-            <p className="text-gray text-base font-medium">Feels like: </p>
-            <p
-              className={`text-xl ml-2 font-bold ${getColorByTemperature(
-                weather.main.feels_like,
-              )}`}
-            >
-              {weather.main.feels_like}° c
-            </p>
-          </div>
-          <div className="flex items-center mt-2">
-            <p className="text-gray text-base font-medium">Humidity: </p>
-            <p className="text-white text-xl ml-2 font-bold">{weather.main.humidity}%</p>
-          </div>
-          <div className="flex items-center mt-2">
-            <p className="text-gray text-base font-medium">Wind speed: </p>
-            <p className="text-white text-xl ml-2 font-bold">{weather.wind.speed} m/s</p>
+          <div className="flex items-start flex-wrap gap-2.5 flex-col md:items-end md:gap-1">
+            <div className="flex items-center">
+              <p className="text-gray text-base font-medium">Temperature: </p>
+              <p
+                className={`text-lg ml-2 font-bold md:text-xl ${getColorByTemperature(
+                  weather.main.temp,
+                )}`}
+              >
+                {weather.main.temp}° c
+              </p>
+            </div>
+            <div className="flex items-center mt-2">
+              <p className="text-gray text-base font-medium">Feels like: </p>
+              <p
+                className={`text-lg ml-2 font-bold md:text-xl ${getColorByTemperature(
+                  weather.main.feels_like,
+                )}`}
+              >
+                {weather.main.feels_like}° c
+              </p>
+            </div>
+            <div className="flex items-center mt-2">
+              <p className="text-gray text-base font-medium">Humidity: </p>
+              <p className="text-white text-lg ml-2 font-bold md:text-xl">
+                {weather.main.humidity}%
+              </p>
+            </div>
+            <div className="flex items-center mt-2">
+              <p className="text-gray text-base font-medium">Wind speed: </p>
+              <p className="text-white text-lg ml-2 font-bold md:text-xl">
+                {weather.wind.speed} m/s
+              </p>
+            </div>
           </div>
         </div>
       )}
