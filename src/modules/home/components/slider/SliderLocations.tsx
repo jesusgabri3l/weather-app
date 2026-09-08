c2d99820b890719e5f9b077692946baa1a9e4dc6
jesusgabri3l <jesus15202009@gmail.com>
2026-09-07 21:35:08 -0500

    fix: unwrap react-slick default export broken by Rolldown CJS interop

--- src/modules/home/components/slider/SliderLocations.tsx
+++ src/modules/home/components/slider/SliderLocations.tsx
@@ -1,10 +1,15 @@
 import { useSelector } from 'react-redux';
-import Slider from 'react-slick';
+import ReactSlick from 'react-slick';
 
 import BaseAlert from '../../../../components/base/BaseInfoAlert';
 import type { RootState } from '../../../../store/store';
 import getSliderSettings from './settingsSlider';
 import SliderLocationsItem from './sliderItem/SliderLocationsItem';
+
+// react-slick is CJS-only; Vite's Rolldown dep optimizer doesn't unwrap its
+// `export default` (it stays as { __esModule, default: Slider } instead of
+// the component itself), so the real component has to be pulled out by hand.
+const Slider = (ReactSlick as unknown as { default?: typeof ReactSlick }).default ?? ReactSlick;
 
 function SliderLocations() {
   const locations = useSelector((state: RootState) => state.location.yourLocations);
